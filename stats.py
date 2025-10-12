ea5c03fae294c3f92bb5d6e94f747fdb096b1235
rexgeodeo <177790025+rexgeodeo7@users.noreply.github.com>
2025-10-12 18:05:18 -0500

    E: added count sorting function to stats.py (sort_dict)

--- stats.py
+++ stats.py
@@ -7,6 +7,7 @@ def get_words(text):
 
     return words_count
 
+
 def get_characters(text):
     characters = {}
 
@@ -20,3 +21,18 @@ def get_characters(text):
             characters[lower_letter] += 1
 
     return characters
+
+def sort_helper(item):
+    return item["num"]
+
+
+def sort_dict(characters_dict):
+    char_num_dict_list = []
+
+    # breaks the dict of char->value into two dicts of char->char and value->count inside a list (2 dicts per index)
+    for key in characters_dict:
+        char_num_dict_list.append({"char": key, "num":characters_dict[key]})
+    
+    # sorts the list from highest to lowest
+    char_num_dict_list.sort(reverse=True, key=sort_helper)
+    return char_num_dict_list
