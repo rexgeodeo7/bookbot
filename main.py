239d37676d5db463efc53c12ba8ecdf7e9c02efc
rexgeodeo <177790025+rexgeodeo7@users.noreply.github.com>
2025-10-12 17:01:54 -0500

    D: added get_characters to stats.py and updated main.py to print accordingly

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from stats import get_words
+from stats import get_words, get_characters
 
 def main():
     book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    words = get_words(text)
-    print(f"Found {words} total words")
 
+    # get text (string) to be used by the methods
+    text = get_book_text(book_path)
+    
+    characters_dict = get_characters(text)
+
+    words = get_words(text)
+
+    print(characters_dict)
+    print(f"Found {words} total words")
+    
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-    
-
-
-
 
 main()
--- stats.py
+++ stats.py
@@ -6,3 +6,17 @@ def get_words(text):
         words_count += 1
 
     return words_count
+
+def get_characters(text):
+    characters = {}
+
+    for letter in text:
+        lower_letter = letter.lower()
+
+        if lower_letter not in characters:
+            characters[lower_letter] = 1
+        
+        else:
+            characters[lower_letter] += 1
+
+    return characters
