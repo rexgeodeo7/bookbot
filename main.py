a67469535648bfee083a4e5e74e6c02bab7504d4
rexgeodeo <177790025+rexgeodeo7@users.noreply.github.com>
2025-10-12 18:06:35 -0500

    F: restructured main.py and added print() report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_words, get_characters
+from stats import get_words, get_characters, sort_dict
 
 def main():
     book_path = "books/frankenstein.txt"
@@ -6,13 +6,21 @@ def main():
     # get text (string) to be used by the methods
     text = get_book_text(book_path)
     
+    # make the char->value dict and then sort it
     characters_dict = get_characters(text)
+    sorted_dict = sort_dict(characters_dict)
 
+    # word count
     words = get_words(text)
 
-    print(characters_dict)
+    # print() structure
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {words} total words")
-    
+    print("--------- Character Count -------")
+    print(sorted_dict)
+    print("============= END ===============")
 
 def get_book_text(path):
     with open(path) as f:
